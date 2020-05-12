500e05e553bbcba19d9010f56604c5e29f746408
russ3llwong <russellwong97@hotmail.com>
2020-05-12 16:51:03 -0700

    Implemented email/password sign-up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -22,8 +22,30 @@ class App extends React.Component {
 
   componentDidMount() {
     // firebase will keep track of user session
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+
+        // gets documentSnapshot
+        userRef.onSnapshot(snapShot => {
+
+          this.setState({
+            currentUser: {
+              id: snapShot.id, // uid
+              ...snapShot.data() // other data of user
+            }
+          }, () => {
+            // callback because setState is async
+            console.log(this.state);
+          });
+
+        });
+
+      } else {
+        // set currentUser to null
+        this.setState({ currentUser: userAuth })
+      }
+      
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,12 +13,40 @@ const config = {
     measurementId: "G-2FWTR4G6NJ"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalDate) => {
+  if (!userAuth) return;
+
+  // returns a firebase Doc reference object
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  // create user in db if it does not exist
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt= new Date();
+    // async request to database
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalDate
+      })
+    } catch (error) {
+      console.log('error creating user, ', error.message)
+    }
+  }
+
+  return userRef
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account '}); // to always trigger google pop up
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
