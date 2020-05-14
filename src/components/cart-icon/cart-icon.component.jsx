d8be20aa0a815f15065deba334b0d7a6307658c1
russ3llwong <russellwong97@hotmail.com>
2020-05-13 21:21:19 -0700

    Added selectors for cart items to avoid re-renders

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cartActions';
-
+import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon' onClick={toggleCartHidden}/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -18,4 +18,9 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    // selector
+    itemCount: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cartSelectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from 'reselect';
+
+// input selector
+// takes the whole state and return a slice of it
+const selectCart = state => state.cart;
+
+// output selector
+// uses createSelector
+export const selectCartItems = createSelector(
+    [selectCart], // collection of input selectors for 1st arg
+    (cart) => cart.cartItems // desired output for 2nd arg
+)
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumalatedQuantity, cartItem) => 
+        accumalatedQuantity + cartItem.quantity, 0)
+)
+
