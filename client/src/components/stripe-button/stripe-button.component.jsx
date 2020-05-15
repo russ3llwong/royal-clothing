d643cd19d249bcf3079680e4cdd7a720c0eb29da
russ3llwong <russellwong97@hotmail.com>
2020-05-15 10:44:11 -0700

    Implemented backend for stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100; // Stripe wants price in cents
@@ -7,9 +8,24 @@ const StripeCheckoutButton = ({ price }) => {
     
     // on success callback after submit
     const onToken = token => {
-        console.log(token); // pass token to backend to process
-        alert('Payment Successful');
-    }
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('Payment Successful!');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
 
     return (
         <StripeCheckout 
